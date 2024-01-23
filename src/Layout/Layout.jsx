de8742b086bce83640d85073420ca182472a1d4d
Abhijitsarker <abhijitsarker03@gmail.com>
2024-01-23 17:17:41 +0600

    basic layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,9 +3,12 @@ import Home from '../Pages/Home/Home';
 
 const Layout = () => {
     return (
-        <div>
-            <p className='text-red-500'>hellofgfg</p>
-            <Home></Home>
+        <div className='bg-[#011221] p-6 h-screen'>
+            <div className='bg-[#011627] border border-slate-800 shadow-2xl h-full rounded-lg'>
+
+                <p className='text-red-500'>hellofgfg</p>
+                <Home></Home>
+            </div>
         </div>
     );
 };
