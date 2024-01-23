82b3f8df751a4e2c26a760941be5f890205ce23d
Abhijitsarker <abhijitsarker03@gmail.com>
2024-01-23 21:50:10 +0600

    header footer set up

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import Home from '../Pages/Home/Home';
+import Header from '../Components/Header/Header';
+import Footer from '../Components/Footer/Footer';
 
 const Layout = () => {
     return (
         <div className='bg-[#011221] p-6 h-screen'>
-            <div className='bg-[#011627] border border-slate-800 shadow-2xl h-full rounded-lg'>
+            <div className='bg-[#011627] border border-slate-800 shadow-2xl h-full rounded-lg flex flex-col justify-between'>
+                <div className='bg-red-950 rounded-t-lg'>
+                    <Header></Header>
+                </div>
 
-                <p className='text-red-500'>hellofgfg</p>
-                <Home></Home>
+
+                <div className='bg-red-950 rounded-b-lg'>
+                    <Footer></Footer>
+                </div>
+
             </div>
         </div>
     );
