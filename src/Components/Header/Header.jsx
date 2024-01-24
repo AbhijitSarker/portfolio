34f466b3805476a9df7048758585f0899a646dc4
Abhijitsarker <abhijitsarker03@gmail.com>
2024-01-24 15:42:20 +0600

    making header responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { GiTireIronCross } from "react-icons/gi";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className='flex flex-row justify-between border-b border-b-slate-700 h-12 text-sm'>
+        <div className='relative flex flex-row justify-between border-b border-b-slate-700 h-12 text-sm'>
             {/* name */}
-            <div className='border-r border-r-slate-700 px-8 w-64 flex items-center'>
+            <div className='md:border-r md:border-r-slate-700 px-8 md:w-64 flex items-center'>
                 <p>Abhijit Sarker</p>
             </div>
 
-            {/* links */}
-            <div className='flex flex-row justify-between w-full'>
+
+            {/* links for small device */}
+            <div className={`flex flex-col items-center absolute top-12 w-full bg-slate-300 md:hidden ${open ? 'flex' : 'hidden'}`}>
+
+                <div className='flex items-center py-5 border-b border-b-slate-700'>
+                    <Link to={'/'}>
+                        _hello-world
+                    </Link>
+                </div>
+                <Link to={'/about-me'}>
+                    <div className='flex items-center py-5 border-b border-b-slate-700'>_about-me</div>
+                </Link>
+                <Link to={'/my-projects'}>
+                    <div className='flex items-center py-5 border-b border-b-slate-700'>_my-projects</div>
+                </Link>
+                <Link to={'/contact-me'}>
+                    <div className='flex items-center py-5 border-b border-b-slate-700'>_hello</div>
+                </Link>
+
+            </div>
+
+            {/* links for large device*/}
+            <div className='hidden md:flex flex-row justify-between w-full'>
                 <div className='flex'>
 
                     <Link to={'/'}>
@@ -29,13 +53,19 @@ const Header = () => {
                 </div>
 
                 {/* contact me */}
-                {/* <div className='border-l border-l-slate-700 px-8 w-56 flex items-center'> */}
                 <Link to={'/contact-me'}>
                     <div className='flex items-center h-full border-l  border-l-slate-700 px-8 hover:text-white hover:bg-[#0F2031]'>_contact-me</div>
                 </Link>
-                {/* </div> */}
+
             </div>
-        </div>
+
+
+            <div className='md:hidden flex items-center pr-4' onClick={() => setOpen(!open)}>
+                {
+                    open ? <GiTireIronCross className='text-4xl ' /> : <FaBarsStaggered className='text-4xl ' />
+                }
+            </div>
+        </div >
     );
 };
 
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,14 +4,16 @@ import Footer from '../Components/Footer/Footer';
 
 const Layout = () => {
     return (
-        <div className='bg-[#03101B] p-8 h-screen  font-firaCode text-font'>
+        <div className='bg-[#03101B] p-3 md:p-8 h-screen  font-firaCode text-font'>
             <div className='bg-[#011627] border border-slate-700 shadow-2xl h-full rounded-lg flex flex-col justify-between'>
 
                 <div className=' rounded-t-lg'>
                     <Header></Header>
                 </div>
+                <div className='h-full'>
 
-                <Outlet></Outlet>
+                    <Outlet></Outlet>
+                </div>
 
                 <div className='bg-red-950 rounded-b-lg'>
                     <Footer></Footer>
