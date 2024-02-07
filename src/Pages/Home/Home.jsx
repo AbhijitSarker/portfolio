2cde5b95cfe00a0a52e19999f32ad8b30043954c
AbhijitSarker <abhijitsarker03@gmail.com>
2024-02-07 22:45:08 +0600

    updated home and assests

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,41 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { motion } from "framer-motion";
 import green from '../../Assets/bg/Green.png';
 import blue from '../../Assets/bg/Blue.png';
 import TicTacToe from '../../Components/TicTacToe/TicTacToe';
 import SnakeGame from '../../Components/SnakeGame/SnakeGame';
 const Home = () => {
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        setIsVisible(true);
-    }, []);
 
     return (
-        <div className=' h-full w-full'>
-            <img className='absolute  h-full left-0' src={green} alt="" />
-            <img className='absolute  h-full right-0' src={blue} alt="" />
-            <SnakeGame></SnakeGame>
-            {/* <motion.div
-                initial={{ opacity: 0, y: -20, scale: 0.8 }}
-                animate={{ opacity: 1, y: 0, scale: 1 }}
-                exit={{ opacity: 0, y: -20, scale: 0.8 }}
-                transition={{ duration: 0.5, ease: "easeInOut" }}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                drag
-                dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
-                dragElastic={0.2}
-                className="my-component  text-white p-4 rounded-md"
-            >
-                <h1 className="text-lg font-bold mb-2">Animated Component</h1>
-                <p>This component has various Framer Motion animations applied to it.</p>
-            </motion.div> */}
+        <div className='relative z-auto h-full w-full flex '>
+            <div className='w-full h-full home-bg-green'>
+                <h1>Hello, I am Abhijit Sarker</h1>
+            </div>
+            <div className='w-full h-full home-bg-blue'>
 
+            </div>
 
-            {/* <div className={` transition-transform duration-500 ease-in-out transform ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-5 opacity-0'}`}>
-                <p>Content goes here</p>
-            </div> */}
+            {/* <img className='absolute h-full z-10 right-0 w-full top-0' src={green} alt="" />
+            <img className='absolute h-full right-0 top-0 w-full' src={blue} alt="" /> */}
+
         </div>
 
     );
