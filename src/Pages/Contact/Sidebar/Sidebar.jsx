02b2fb037eccb98f7733335cfb08241859cb6b8e
AbhijitSarker <abhijitsarker03@gmail.com>
2024-02-29 18:03:58 +0600

    sidebar

--- src/Pages/Contact/Sidebar/Sidebar.jsx
+++ src/Pages/Contact/Sidebar/Sidebar.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Sidebar = () => {
     return (
         <div className='h-full md:border-r md:border-r-slate-700 w-80'>
-            <div>
-                contacts
+            <div className=''>
+                <div>
+                    <h3 className='pl-8 py-3 md:border-b md:border-b-slate-700 text-white'>_contacts</h3>
+                </div>
+                <div className='pl-8 py-3 space-y-2'>
+                    <div className='flex items-center gap-2'>
+                        <MdEmail />
+                        <a href="mailto:user@example.com">abhijit.srker@gmail.com</a> <br />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <FaPhoneAlt />
+                        <a href="tel:+1234567890">+8801703290219</a>
+                    </div>
+                </div>
             </div>
+            <div className=''>
+                <div>
+                    <h3 className='pl-8 py-3 md:border-b md:border-b-slate-700 text-white'>Contacts</h3>
+                </div>
+                <div className='pl-8 py-3 space-y-2'>
+                    <div className='flex items-center gap-2'>
+                        <MdEmail />
+                        <a href="mailto:user@example.com">abhijit.srker@gmail.com</a> <br />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <FaPhoneAlt />
+                        <a href="tel:+1234567890">+8801703290219</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
