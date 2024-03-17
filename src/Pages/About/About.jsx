b2317f36973be1e56a6f76a205fc0d35c380daf5
Abhijitsarker <abhijitsarker03@gmail.com>
2024-03-17 18:44:55 +0600

    about set up

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import SideBar from './SideBar/SideBar';
 
 const About = () => {
     return (
-        <div>
+        <div className='w-full h-full'>
             <Helmet>
                 <title>About</title>
                 <link rel="canonical" href="https://www.tacobell.com/" />
             </Helmet>
+
+            {/* sidebar */}
+            <div>
+                <SideBar></SideBar>
+            </div>
+
+            {/* about */}
+            <div>
+
+            </div>
+
+            {/* code block */}
+            <div>
+
+            </div>
+
         </div>
     );
 };
