943bb7b723f649b45a0949f17c075ba6a100f48d
WP@as@g#01 <abhijitsarker03@gmail.com>
2024-08-08 17:37:38 +0600

    new routes added

---
+++ src/Pages/About/SideBar/Professional.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { Outlet } from 'react-router-dom';
+
+const Professional = () => {
+    const [isOpen, setIsOpen] = useState([true, false, false]);
+
+    const toggleAccordion = (index) => {
+        const newIsOpen = [...isOpen];
+        newIsOpen[index] = !newIsOpen[index];
+        setIsOpen(newIsOpen);
+    };
+    const a = 1;
+    const b = 2;
+    return (
+        <div>
+            <div className="w-64 mx-auto">
+                <div className="space-y-2">
+                    <div className="">
+                        <div
+                            className="flex items-center justify-between cursor-pointer w-full md:border-b md:border-b-slate-700 pr-4"
+                            onClick={() => toggleAccordion(0)}
+                        >
+                            <span className='w-full pl-8 py-3  text-white'>_Professional-info</span>
+                            <svg
+                                className={`w-4 h-4 transition-transform ${isOpen[0] ? "transform rotate-180" : ""
+                                    }`}
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 18a1 1 0 0 1-.707-.293l-8-8a1 1 0 0 1 1.414-1.414L10 15.586l7.293-7.293a1 1 0 0 1 1.414 1.414l-8 8A1 1 0 0 1 10 18z"
+                                />
+                            </svg>
+                        </div>
+                        {isOpen[0] && (
+                            <div className="p-4 space-y-2">
+                                <div className='flex items-center gap-2'>
+                                    <p className='text-[#E99287]'><MdEmail /></p>
+                                    <a>Experience</a> <br />
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <p className='text-[#43D9AD]'><FaPhoneAlt /></p>
+                                    <a>Hard Skills</a>
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <p className='text-[#3A49A4]'><FaPhoneAlt /></p>
+                                    <a>Soft Skills</a>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <div
+                            className="flex items-center justify-between cursor-pointer w-full md:border-b md:border-b-slate-700 pr-4"
+                            onClick={() => toggleAccordion(1)}
+                        >
+                            <span className='w-full pl-8 py-3  text-white'>_also-find-me-on</span>
+                            <svg
+                                className={`w-4 h-4 transition-transform ${isOpen[1] ? "transform rotate-180" : ""
+                                    }`}
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 18a1 1 0 0 1-.707-.293l-8-8a1 1 0 0 1 1.414-1.414L10 15.586l7.293-7.293a1 1 0 0 1 1.414 1.414l-8 8A1 1 0 0 1 10 18z"
+                                />
+                            </svg>
+                        </div>
+                        {isOpen[1] && (
+                            <div className="p-4 space-y-2">
+                                <div className='flex items-center gap-2'>
+                                    <MdEmail />
+                                    <a href="mailto:abhijit.srker@gmail.com">l</a> <br />
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <FaPhoneAlt />
+                                    <a href="tel:+8801703290219">+8801703290219</a>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Professional;
--- src/Pages/About/SideBar/SideBar.jsx
+++ src/Pages/About/SideBar/SideBar.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import { MdEmail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import Professional from './Professional';
 
 const Sidebar = () => {
 
-    const [isOpen, setIsOpen] = useState([true, false, false]);
+    // const [isOpen, setIsOpen] = useState([true, false, false]);
 
-    const toggleAccordion = (index) => {
-        const newIsOpen = [...isOpen];
-        newIsOpen[index] = !newIsOpen[index];
-        setIsOpen(newIsOpen);
-    };
-    const a = 1;
-    const b = 2;
+    // const toggleAccordion = (index) => {
+    //     const newIsOpen = [...isOpen];
+    //     newIsOpen[index] = !newIsOpen[index];
+    //     setIsOpen(newIsOpen);
+    // };
+    // const a = 1;
+    // const b = 2;
     return (
         <motion.div
             initial={{ opacity: 0, x: -10 }} // Initial state
@@ -22,78 +22,12 @@ const Sidebar = () => {
             transition={{ duration: .5 }}
             className='h-full md:border-r md:border-r-slate-700 w-80 flex'
         >
-            <div className='w-[60px] border-r border-r-slate-700'>
-                <div className='h-14 flex justify-center items-center text-[#607b9675]'><FaPhoneAlt /></div>
-                <div className='h-14 flex justify-center items-center text-[#607b96]'><FaPhoneAlt /></div>
-                <Link to='/about'> <div>dfsg</div></Link>
-                <Link to="/about/experience"><div>sdg</div></Link>
-                <Link> <div>uk</div></Link>
-            </div>
-            <div className="max-w-md mx-auto">
-                <div className="space-y-2">
-                    <div className="">
-                        <div
-                            className="flex items-center justify-between cursor-pointer w-full md:border-b md:border-b-slate-700 pr-4"
-                            onClick={() => toggleAccordion(0)}
-                        >
-                            <span className='w-full pl-8 py-3  text-white'>_contacts</span>
-                            <svg
-                                className={`w-4 h-4 transition-transform ${isOpen[0] ? "transform rotate-180" : ""
-                                    }`}
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a1 1 0 0 1-.707-.293l-8-8a1 1 0 0 1 1.414-1.414L10 15.586l7.293-7.293a1 1 0 0 1 1.414 1.414l-8 8A1 1 0 0 1 10 18z"
-                                />
-                            </svg>
-                        </div>
-                        {isOpen[0] && (
-                            <div className="p-4 space-y-2">
-                                <div className='flex items-center gap-2'>
-                                    <MdEmail />
-                                    <a href="mailto:abhijit.srker@gmail.com">abhijit.srker@gmail.com</a> <br />
-                                </div>
-                                <div className='flex items-center gap-2'>
-                                    <FaPhoneAlt />
-                                    <a href="tel:+8801703290219">+8801703290219</a>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-
-
-                    <div>
-                        <div
-                            className="flex items-center justify-between cursor-pointer w-full md:border-b md:border-b-slate-700 pr-4"
-                            onClick={() => toggleAccordion(1)}
-                        >
-                            <span className='w-full pl-8 py-3  text-white'>_also-find-me-on</span>
-                            <svg
-                                className={`w-4 h-4 transition-transform ${isOpen[1] ? "transform rotate-180" : ""
-                                    }`}
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a1 1 0 0 1-.707-.293l-8-8a1 1 0 0 1 1.414-1.414L10 15.586l7.293-7.293a1 1 0 0 1 1.414 1.414l-8 8A1 1 0 0 1 10 18z"
-                                />
-                            </svg>
-                        </div>
-                        {isOpen[1] && (
-                            <div className="p-4">
-                                Content for section 2
-                            </div>
-                        )}
-                    </div>
-                </div>
+            <div className='w-16 border-r border-r-slate-700'>
+                <Link to='/about'><div className='h-14 flex justify-center items-center text-[#607b9675]'><FaPhoneAlt /></div></Link>
+                <Link to="/about/experience"><div className='h-14 flex justify-center items-center text-[#607b96]'><FaPhoneAlt /></div></Link>
             </div>
 
-
-
-
+            <Professional></Professional>
 
         </motion.div>
     );
